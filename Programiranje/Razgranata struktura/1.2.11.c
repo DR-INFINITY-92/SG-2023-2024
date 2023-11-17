5d5f298676701709fb9ac0f20af0ea9aa89c6f27
DR_INFINITY <d.rajkovicrs@gmail.com>
2023-11-17 10:40:03 +0100

    x

---
+++ Programiranje/Razgranata struktura/1.2.11.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main ()
+{
+    int n,j,s,h,d;
+    p:
+    printf("unesi celi cetvrocifreni broj broj\n");
+    scanf("%d",&n);
+    if(n>999 && n<10000)
+    {
+       j=n%10;
+       n=n/10;
+       d=n%10;
+       n=n/10;
+       s=n%10;
+       n=n/10;
+       h=n%10;
+       if(j>d && j>s && j>h)
+            printf("najveca cifra je %d",j);
+        else
+            if(d>j && d>s && d>h)
+                printf("najveca cifra je %d",d);
+            else
+                if(s>d && s>j && s>h)
+                    printf("najveca cifra je %d",s);
+                else
+                    printf("najveca cifra je %d",h);
+
+    }
+    else
+    {
+        printf("niste uneli cetvorocifreni broj unesi opet/n");
+        goto p;
+    }
+    return 0;
+}
