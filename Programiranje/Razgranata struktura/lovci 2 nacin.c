688ca1a5a89a03b7fcd75c5952cd11b82123cf68
DR_INFINITY <d.rajkovicrs@gmail.com>
2023-11-24 12:04:14 +0100

    x

---
+++ Programiranje/Razgranata struktura/lovci 2 nacin.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int x1,y1,x2,y2;
+    p:
+    printf("unesi x i y koordinatu prvog lovca\n");
+    scanf("%d%d",&x1,&y1);
+    printf("unesi x i y koordinatu drugog lovca\n");
+    scanf("%d%d",&x2,&y2);
+    if(x1<1 || x2<1 || y1<1 || y2<1 ||x1>8 || x2>8|| y1>8 || y2>8)
+    {
+        printf("greska koordinate figura moraju biti u opsegu od 1-8\n");
+        goto p;
+    }
+    if(x1==x2 && y1==y2)
+    {
+            printf("greska nemogu da figure budu na istom polju\n");
+            goto p;
+    }
+    if(abs(x2-x1)==abs(y2-y1))
+        printf("lovci se napadaju\n");
+    else
+        printf("lovci se ne napadaju\n");
+
+    return 0;
+}
