0abc53c27be688d16d665ff373e07f28b18f6100
DR_INFINITY <d.rajkovicrs@gmail.com>
2023-12-05 13:45:50 +0100

    x

--- Programiranje/Petlje/1.3.6.c
+++ Programiranje/Petlje/1.3.6.c
@@ -2,16 +2,23 @@
 #include<stdlib.h>
 int main()
 {
- int n,x,i,s;
- printf("Unesi broj\n");
- scanf("%d",&x);
- printf("Unesi stepen broja n\n");
+ int n,i;
+ float x,s;
+ printf("Unesi broj x\n");
+ scanf("%f",&x);
+ p:
+ printf("Unesi broj n koji predstavlja stepen\n");
  scanf("%d",&n);
+ if(n<0)
+ {
+   printf("Greska: pogresan unos\n");
+   goto p;
+ }
  s=1;
- for(i=0;i<n;i++)
+ for(i=0;i<n;i++) /*n=3 a x=2 s=8 i=0,1,2,3  i=0, s=2 i=1 s=4 i=2 s=8*/
  {
     s=s*x;
  }
- printf("%d",s);
+ printf("%.2f",s);
     return 0;
 }    
