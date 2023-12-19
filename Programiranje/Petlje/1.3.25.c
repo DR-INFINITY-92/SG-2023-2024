e85127652cacc15a32fa6f1e70a5c70bf5d486e5
DR_INFINITY <d.rajkovicrs@gmail.com>
2023-12-19 13:18:35 +0100

    x

--- Programiranje/Petlje/1.3.25.c
+++ Programiranje/Petlje/1.3.25.c
@@ -2,14 +2,20 @@
 #include<stdlib.h>
 int main() 
 {
-    int n,m=0,o=1;
+    int n,m=0,o=1,b=1,c;
     printf("unesi broj n\n");
     scanf("%d",&n);
     while (n>0)
     {
-        if(n<10)
-        break;
-    
+        c=n%10;
+        if(b%2!=0)
+        {
+            m=m+c*o;
+            o=o*10;
+        }
+        b++;
+        n=n/10;
     }
+    printf("%d\n",m);
 
 }
