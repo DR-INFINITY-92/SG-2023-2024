e53a2f1153a8eaac3d96a9d51ecc47ef7dddd4a1
DR_INFINITY <d.rajkovicrs@gmail.com>
2023-12-22 12:52:21 +0100

    x

---
+++ Programiranje/Petlje/tablica mnozenja.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,n,j;
+    printf("unesi n\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf(" %d ",i*j);
+        }
+        printf("\n");
+    }
+
+}
